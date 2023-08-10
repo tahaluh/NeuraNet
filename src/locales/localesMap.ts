7466716909c4ef353ceaa2526737c5c82b02c229
tahaluh <thauanlucascpl@gmail.com>
2023-08-10 12:50:19 -0300

    feat: esEs locale added

--- src/locales/localesMap.ts
+++ src/locales/localesMap.ts
@@ -1,10 +1,12 @@
 import { Locale } from "../types/types";
 import ptBrLocale from "./pt-br.json";
 import enUsLocale from "./en-us.json";
+import esEsLocale from "./es-es.json";
 
 const localesMap: Record<string, Locale> = {
   "pt-br": ptBrLocale,
   "en-us": enUsLocale,
+  "es-es": esEsLocale,
 };
 
 export default localesMap;
