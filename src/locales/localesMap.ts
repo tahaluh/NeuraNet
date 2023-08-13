9753e177acc89bbd63dfdf090ef555f964ed76ca
tahaluh <thauanlucascpl@gmail.com>
2023-08-12 22:21:59 -0300

    feat: frFr locale added

--- src/locales/localesMap.ts
+++ src/locales/localesMap.ts
@@ -2,11 +2,13 @@ import { Locale } from "../types/types";
 import ptBrLocale from "./pt-br.json";
 import enUsLocale from "./en-us.json";
 import esEsLocale from "./es-es.json";
+import frFrLocale from "./fr-fr.json";
 
 const localesMap: Record<string, Locale> = {
   "pt-br": ptBrLocale,
   "en-us": enUsLocale,
   "es-es": esEsLocale,
+  "fr-fr": frFrLocale,
 };
 
 export default localesMap;
